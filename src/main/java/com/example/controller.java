b146f0465affcb97688a48e8f6d897d312ded06b
Abhishek <179099403+abhishek1986444@users.noreply.github.com>
2025-12-23 12:31:49 +0530

    add TextFields for taking input from fxml file.
    left with validation of input

--- src/main/java/com/example/controller.java
+++ src/main/java/com/example/controller.java
@@ -4,12 +4,45 @@ import java.sql.SQLException;
 
 import java.sql.*;
 
-import java.sql.PreparedStatement; 
+import java.sql.PreparedStatement;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+
+import javafx.scene.control.TextField;
+
+
+import javafx.scene.control.PasswordField ;
+
+import javafx.scene.control.Alert;
+
+
+import javafx.scene.control.Alert.AlertType;
+
 
 
 
 @SuppressWarnings(  "unused" )
+
+
+
+
 public class controller {
+
+// ----------------- fxml fields  - ------
+
+
+
+    @FXML  private TextField useridField;
+
+    @FXML private TextField usernameField ;
+
+
+    @FXML private TextField mailField ;
+
+    @FXML private PasswordField passwordField ;
+
+
 
     private struct object ;
 
@@ -31,14 +64,28 @@ public  void  register() throws SQLException  // add register function in the co
  
 {
 
+ String user_id = useridField.getText().trim();
+
+
+
+String mail_id = mailField.getText().trim() ; 
+
+
+
+String password = passwordField.getText().trim();
+
+
+String username = usernameField.getText().trim() ;
+
+
+
+
+
+
 
 String userinput = " insert into userrecord (userid , mailid , password, username ) values ( ?, ? , ?  , ? )  ;   " ; 
 
 PreparedStatement ps = object.connection.prepareStatement(userinput);
-
-
-String  user_id = "" , mail_id = "" , password = "" , username = "" ;
-
 
 
 
